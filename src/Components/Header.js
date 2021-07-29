a1e505961abfaebdb417205a3034019a214e34f6
Daouda Oubri <daouda@declic.africa>
2021-07-29 16:32:00 +0000

    feat/add-authentification

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 className={"text-center"}>Todo</h1>
+
+      <header>
+        <button>Sign Out</button>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
