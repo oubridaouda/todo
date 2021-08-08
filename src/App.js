f2d9d87d0df26075c1bde92acef160b24103d41f
Daouda Oubri <daouda@declic.africa>
2021-08-08 23:39:33 +0000

    feat:login design

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ import TodosList from "./Components/TodoList";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { TodoForm } from "./Components/Form";
 import Login from "./Components/Login";
-import LogOut from "./Components/LogOut";
-import Signup from "./Components/Signup";
-
-import firebase from "firebase";
 
 const App = () => {
   const [todoId, setTodoId] = useState("");
@@ -21,24 +17,23 @@ const App = () => {
     <div className="App">
       <div className="app-wrapper">
         <div>
-          <Header />
-        </div>
-
-        <div>
           {todoUser ? (
             <div>
-              <LogOut setTodoUser={setTodoUser} />
+              <Header todoUserData={setTodoUser} />
               <div>
                 <TodoForm
                   id={todoId}
                   name={todoName}
                   setTodoName={setTodoName}
+                  todoUserData={setTodoUser}
                 />
               </div>
               <TodosList setTodoName={setTodoName} setTodoId={setTodoId} />
             </div>
           ) : (
-            <Login todoUserData={setTodoUser} />
+            <div className="login">
+              <Login todoUserData={setTodoUser} />
+            </div>
           )}
         </div>
       </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,21 @@
 import React from "react";
+import firebase from "firebase";
 
-const Header = () => {
+const Header = ({ todoUserData }) => {
+  const handleLogout = () => {
+    todoUserData("");
+    firebase.auth().signOut();
+  };
   return (
-    <div className="header">
-      <h1 className={"text-center"}>Todo</h1>
+    <div className="header header-fixed">
+      <div className="navbar container">
+        <div className="logo">
+          <a href="#home">TODO</a>
+        </div>
+        <button onClick={handleLogout} className="btn btn-warning">
+          Logout
+        </button>
+      </div>
     </div>
   );
 };
